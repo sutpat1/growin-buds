48e8b127f75a025237c8de8b15088999ccf9b8b7
gmiani <130406466+gmiani@users.noreply.github.com>
2023-04-27 12:46:50 -0500

    fixed my app.js

--- growin-buds/App.js
+++ growin-buds/App.js
@@ -1,91 +1,96 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import CheckBox from 'expo-checkbox';
-import { useState } from 'react';
+import Welcome from "./screens/Welcome";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
+import Home from "./screens/Home";
+import PlantProfile from "./screens/PlantProfile";
+import CreatePlantProfile from "./screens/CreatePlantProfile";
+import UserProfile from "./screens/UserProfile";
+import Friends from "./screens/Friends";
+import Search from "./screens/Search";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
-  const [isChecked1, setChecked1] = useState(false);
-  const [isChecked2, setChecked2] = useState(false);
-  const [isChecked3, setChecked3] = useState(false);
-  const [isChecked4, setChecked4] = useState(false);
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function NavBar() {
   return (
-    <View style={{backgroundColor: '#fffff5'}}>
-      <View style={{flexDirection: 'row'}}>
-        <Text style={styles.username}>Hi, Grace!</Text>
-        <Image source={require("./assets/userpic.png")} style={{position: 'absolute', left: 225, top: 80}}></Image>
-      </View>
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-      <View style={{flexDirection: 'row'}}>
-        <Image source={require("./assets/pinkplant.png")} style={{alignSelf: 'flex-start', marginTop: 10, marginLeft: 65}}></Image>
-        <Image source={require("./assets/star.png")} style={{marginTop: 120, marginLeft: 85}}></Image>
-      </View>
-
-      <Text style={{fontSize: 18, position: 'absolute', left: 305, top: 247}}>65</Text>
-      <Text style={styles.subheader}>Daily Tasks:</Text>
-
-      
-      <Image source={require("./assets/rectangles/darkgreen.png")} style={{position: 'absolute', left: 33, top: 350}}></Image>
-      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 368}}></Image>
-      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 375}}>Water tulips</Text>
-      <Text style={{fontSize: 18, position: 'absolute', left: 300, top: 377}}>5</Text>
-
-      <Image source={require("./assets/rectangles/green.png")} style={{position: 'absolute', left: 33, top: 450}}></Image>
-      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 468}}></Image>
-      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 475}}>Water rose plant</Text>
-      <Text style={{fontSize: 18, position: 'absolute', left: 300, top: 477}}>5</Text>
-      
-      
-
-      <Image source={require("./assets/rectangles/lightgreen.png")} style={{position: 'absolute', left: 33, top: 550}}></Image>
-      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 568}}></Image>
-      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 565}}>Change soil for</Text>
-      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 585}}>tomato plant</Text>
-      <Text style={{fontSize: 18, position: 'absolute', left: 296, top: 577}}>10</Text>
-      
-
-      <Image source={require("./assets/rectangles/grey.png")} style={{position: 'absolute', left: 33, top: 650}}></Image>
-      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 668}}></Image>
-      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 665}}>Add fertilizer to</Text>
-      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 685}}>carrot plant</Text>
-      <Text style={{fontSize: 18, position: 'absolute', left: 296, top: 677}}>10</Text>
-      
-      <CheckBox value={isChecked1} color="#696969"
-        onValueChange={setChecked1}
-        style={{position: 'absolute', left: 60, top: 375}}
-      />
-
-      <CheckBox value={isChecked2} color="#696969"
-        onValueChange={setChecked2}
-        style={{position: 'absolute', left: 60, top: 475}}
-      />
-
-      <CheckBox value={isChecked3} color="#696969"
-        onValueChange={setChecked3}
-        style={{position: 'absolute', left: 60, top: 575}}
-      />
-
-      <CheckBox value={isChecked4} color="#696969"
-        onValueChange={setChecked4}
-        style={{position: 'absolute', left: 60, top: 675}}
-      />
-      
-    </View>
-  )
+            if (route.name === 'Home') {
+              iconName = focused ? 'home': 'home-outline';
+            } else if (route.name === 'Plants') {
+                iconName = focused ? 'leaf' : 'leaf-outline';
+            } else if (route.name === 'User Profile'){
+                iconName = focused ? 'person' : 'person-outline';  
+            } else if (route.name === 'Friends') {
+                iconName = focused ? 'people' : 'people-outline';
+            } else if (route.name === 'Search') {
+                iconName = focused ? 'search' : 'search-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#8BC060',
+          tabBarInactiveTintColor: 'black',
+        })}
+      >
+        <Tab.Screen 
+          name="Friends"
+          component={Friends}
+        />
+        <Tab.Screen 
+          name="Search"
+          component={Search}
+        />
+        <Tab.Screen 
+            name="Home"
+            component={Home}
+        />
+        <Tab.Screen 
+          name="Plants"
+          component={PlantProfile}
+        />
+        <Tab.Screen 
+          name="User Profile"
+          component={UserProfile}
+        />
+      </Tab.Navigator>
+  );
 }
 
-const styles = StyleSheet.create({
-  username: {
-    fontSize: 40,
-    fontWeight: 500,
-    textAlign: 'left',
-    marginTop: 70,
-    marginLeft: 30,
-  },
-
-  subheader: {
-    marginTop: 15,
-    fontSize: 24,
-    fontWeight: 500,
-    alignSelf: 'center',
-  },
-  
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name = "Welcome"
+          component={Welcome}
+          options={{ headerShown: false}}
+        />
+        <Stack.Screen 
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen 
+          name="SignUp"
+          component={SignUp}
+        />
+        <Stack.Screen 
+          name="Home"
+          component={NavBar}
+          options={{ headerShown: false}}
+        />
+        <Stack.Screen 
+          name="CreatePlantProfile"
+          component={CreatePlantProfile}
+          options={{ headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
--- growin-buds/screens/Home.js
+++ growin-buds/screens/Home.js
@@ -1,22 +1,73 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import CheckBox from 'react-native-check-box';
+import CheckBox from 'expo-checkbox';
+import { useState } from 'react';
 
 export default function App() {
+  const [isChecked1, setChecked1] = useState(false);
+  const [isChecked2, setChecked2] = useState(false);
+  const [isChecked3, setChecked3] = useState(false);
+  const [isChecked4, setChecked4] = useState(false);
   return (
     <View style={{backgroundColor: '#fffff5'}}>
       <View style={{flexDirection: 'row'}}>
         <Text style={styles.username}>Hi, Grace!</Text>
         <Image source={require("./assets/userpic.png")} style={{position: 'absolute', left: 225, top: 80}}></Image>
       </View>
+
       <View style={{flexDirection: 'row'}}>
         <Image source={require("./assets/pinkplant.png")} style={{alignSelf: 'flex-start', marginTop: 10, marginLeft: 65}}></Image>
-        <Image source={require("./assets/taskpoints.png")} style={{marginTop: 120, marginLeft: 85}}></Image>
+        <Image source={require("./assets/star.png")} style={{marginTop: 120, marginLeft: 85}}></Image>
       </View>
+
+      <Text style={{fontSize: 18, position: 'absolute', left: 305, top: 247}}>65</Text>
       <Text style={styles.subheader}>Daily Tasks:</Text>
+
+      
       <Image source={require("./assets/rectangles/darkgreen.png")} style={{position: 'absolute', left: 33, top: 350}}></Image>
+      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 368}}></Image>
+      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 375}}>Water tulips</Text>
+      <Text style={{fontSize: 18, position: 'absolute', left: 300, top: 377}}>5</Text>
+
       <Image source={require("./assets/rectangles/green.png")} style={{position: 'absolute', left: 33, top: 450}}></Image>
+      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 468}}></Image>
+      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 475}}>Water rose plant</Text>
+      <Text style={{fontSize: 18, position: 'absolute', left: 300, top: 477}}>5</Text>
+      
+      
+
       <Image source={require("./assets/rectangles/lightgreen.png")} style={{position: 'absolute', left: 33, top: 550}}></Image>
+      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 568}}></Image>
+      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 565}}>Change soil for</Text>
+      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 585}}>tomato plant</Text>
+      <Text style={{fontSize: 18, position: 'absolute', left: 296, top: 577}}>10</Text>
+      
+
       <Image source={require("./assets/rectangles/grey.png")} style={{position: 'absolute', left: 33, top: 650}}></Image>
+      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 668}}></Image>
+      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 665}}>Add fertilizer to</Text>
+      <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 685}}>carrot plant</Text>
+      <Text style={{fontSize: 18, position: 'absolute', left: 296, top: 677}}>10</Text>
+      
+      <CheckBox value={isChecked1} color="#696969"
+        onValueChange={setChecked1}
+        style={{position: 'absolute', left: 60, top: 375}}
+      />
+
+      <CheckBox value={isChecked2} color="#696969"
+        onValueChange={setChecked2}
+        style={{position: 'absolute', left: 60, top: 475}}
+      />
+
+      <CheckBox value={isChecked3} color="#696969"
+        onValueChange={setChecked3}
+        style={{position: 'absolute', left: 60, top: 575}}
+      />
+
+      <CheckBox value={isChecked4} color="#696969"
+        onValueChange={setChecked4}
+        style={{position: 'absolute', left: 60, top: 675}}
+      />
+      
     </View>
   )
 }
@@ -30,21 +81,11 @@ const styles = StyleSheet.create({
     marginLeft: 30,
   },
 
-  planttype: {
-    fontSize: 30,
-    textAlign: 'center',
-  },
-
   subheader: {
     marginTop: 15,
     fontSize: 24,
     fontWeight: 500,
     alignSelf: 'center',
   },
-
-  regulartext: {
-    fontSize: 18,
-    padding: 5
-  }
   
 });
