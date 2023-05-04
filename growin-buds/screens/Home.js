35ece762f08d9f1647886b1665495e29e99e194b
hxpathak <100104182+hxpathak@users.noreply.github.com>
2023-05-04 18:11:01 -0500

    Update Home.js

--- growin-buds/screens/Home.js
+++ growin-buds/screens/Home.js
@@ -11,39 +11,39 @@ export default function App() {
     <View style={{backgroundColor: '#fffff5'}}>
       <View style={{flexDirection: 'row'}}>
         <Text style={styles.username}>Hi, Grace!</Text>
-        <Image source={require("./assets/userpic.png")} style={{position: 'absolute', left: 225, top: 80}}></Image>
+        <Image source={require("../assets/userpic.png")} style={{position: 'absolute', left: 225, top: 80}}></Image>
       </View>
 
       <View style={{flexDirection: 'row'}}>
-        <Image source={require("./assets/pinkplant.png")} style={{alignSelf: 'flex-start', marginTop: 10, marginLeft: 65}}></Image>
-        <Image source={require("./assets/star.png")} style={{marginTop: 120, marginLeft: 85}}></Image>
+        <Image source={require("../assets/pinkplant.png")} style={{alignSelf: 'flex-start', marginTop: 10, marginLeft: 65}}></Image>
+        <Image source={require("../assets/star.png")} style={{marginTop: 120, marginLeft: 85}}></Image>
       </View>
 
       <Text style={{fontSize: 18, position: 'absolute', left: 305, top: 247}}>65</Text>
       <Text style={styles.subheader}>Daily Tasks:</Text>
 
       
-      <Image source={require("./assets/rectangles/darkgreen.png")} style={{position: 'absolute', left: 33, top: 350}}></Image>
-      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 368}}></Image>
+      <Image source={require("../assets/rectangles/darkgreen.png")} style={{position: 'absolute', left: 33, top: 350}}></Image>
+      <Image source={require("../assets/star.png")} style={{position: 'absolute', left: 285, top: 368}}></Image>
       <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 375}}>Water tulips</Text>
       <Text style={{fontSize: 18, position: 'absolute', left: 300, top: 377}}>5</Text>
 
-      <Image source={require("./assets/rectangles/green.png")} style={{position: 'absolute', left: 33, top: 450}}></Image>
-      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 468}}></Image>
+      <Image source={require("../assets/rectangles/green.png")} style={{position: 'absolute', left: 33, top: 450}}></Image>
+      <Image source={require("../assets/star.png")} style={{position: 'absolute', left: 285, top: 468}}></Image>
       <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 475}}>Water rose plant</Text>
       <Text style={{fontSize: 18, position: 'absolute', left: 300, top: 477}}>5</Text>
       
       
 
-      <Image source={require("./assets/rectangles/lightgreen.png")} style={{position: 'absolute', left: 33, top: 550}}></Image>
-      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 568}}></Image>
+      <Image source={require("../assets/rectangles/lightgreen.png")} style={{position: 'absolute', left: 33, top: 550}}></Image>
+      <Image source={require("../assets/star.png")} style={{position: 'absolute', left: 285, top: 568}}></Image>
       <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 565}}>Change soil for</Text>
       <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 585}}>tomato plant</Text>
       <Text style={{fontSize: 18, position: 'absolute', left: 296, top: 577}}>10</Text>
       
 
-      <Image source={require("./assets/rectangles/grey.png")} style={{position: 'absolute', left: 33, top: 650}}></Image>
-      <Image source={require("./assets/star.png")} style={{position: 'absolute', left: 285, top: 668}}></Image>
+      <Image source={require("../assets/rectangles/grey.png")} style={{position: 'absolute', left: 33, top: 650}}></Image>
+      <Image source={require("../assets/star.png")} style={{position: 'absolute', left: 285, top: 668}}></Image>
       <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 665}}>Add fertilizer to</Text>
       <Text style={{fontSize: 18, position: 'absolute', left: 100, top: 685}}>carrot plant</Text>
       <Text style={{fontSize: 18, position: 'absolute', left: 296, top: 677}}>10</Text>
